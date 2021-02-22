7e553cb05f961268eaf21f6ce0e9a9333b6f6f5d
MarcoDDM <marantal2013@hotmail.com>
2021-02-22 08:12:39 -0300

    trailers

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import YouTube from 'react-youtube';
 import axios from './axios';
 import "./Row.css";
-
+import YouTube from 'react-youtube';
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, settrailerUrl] = useState("");
 
     useEffect(() => {
         async function fetchData() {
@@ -26,6 +27,17 @@ function Row({ title, fetchUrl, isLargeRow }) {
         },
     };
 
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            settrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "").then(url =>  {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                settrailerUrl(urlParams.get('v'));
+            }).cath((error) => console.log(error));
+        }
+    }
+
     return (
         <div className="row">
             <h2>{title} </h2>
@@ -41,7 +53,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
                      alt={movie.name} />
                     ))}
                 </div>
-                <YouTube videoId={trailerUrl} opts={opts} />
+               {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
